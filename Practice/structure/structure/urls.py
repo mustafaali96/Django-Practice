0128b08f595c4711566f724048dd56c8dd50e433
Mustafa Ali <mustafa_socool@hotmail.com>
2020-12-15 16:19:38 -0700

    structure practice

--- Practice/structure/structure/urls.py
+++ Practice/structure/structure/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from structure_app.views import new_structure_list
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    path('list/', new_structure_list)
 ]
--- Practice/structure/structure_app/views.py
+++ Practice/structure/structure_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import new_structure
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def new_structure_list(request):
+    new_list = new_structure.objects.all()
+    context = { 'new_list': new_list}
+    print(new_list)
+    return render(request, 'structure_app/index.html', context)
